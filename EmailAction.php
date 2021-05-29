cc8d57a48e8f42c737d39066a92d2ae6a1ce7447
duvivierbri <duvivierbri@gmail.com>
2021-05-29 17:01:19 -0400

    ...

--- EmailAction.php
+++ EmailAction.php
@@ -1,18 +1,17 @@
 <?php
-/* Can't know for sure if this works until after I put my site on the server... So we'll see! */
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-$fromemail = "$email"
-$myemail = 'duvivierbri@gmail.com';
-$subject = "New email!";
+    $fromemail = "$email";
+    $myemail = "duvivierbri@gmail.com";
+    $subject = "New email!";
 
-$body_message = "New Message: $message";
+    $body_message = "New Message: $message";
 
-$headers = "From: $fromemail \r\n";
-$headers .= "Reply-To: $email \r\n";
+    $headers = "From: $fromemail \n";
+    $headers .= "Reply-To: $email \n";
 
-mail($myemail, $subject, $body_message, $headers);
+    mail($myemail, $subject, $body_message, $headers);
 
 ?>
