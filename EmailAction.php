14a9952a0d90a8737058eb563d2f3bb1748eeba7
duvivierbri <duvivierbri@gmail.com>
2021-05-29 16:42:45 -0400

    Trying to get contact to work

--- EmailAction.php
+++ EmailAction.php
@@ -4,12 +4,13 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
+$fromemail = "$email"
 $myemail = 'duvivierbri@gmail.com';
 $subject = "New email!";
 
 $body_message = "New Message: $message";
 
-$headers = "From: $email \r\n";
+$headers = "From: $fromemail \r\n";
 $headers .= "Reply-To: $email \r\n";
 
 $mail_status = mail($myemail, $subject, $body_message, $headers);
