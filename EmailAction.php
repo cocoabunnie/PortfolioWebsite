eb3345639c826d3bbfda4a6a18362f5cee32191b
duvivierbri <duvivierbri@gmail.com>
2021-05-29 17:26:29 -0400

    ...

--- EmailAction.php
+++ EmailAction.php
@@ -1,16 +1,7 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $fromemail = "test@gmail.com";
-    $myemail = "duvivierbri@gmail.com";
-    $subject = "New email!";
     $body_message = "New Message: $message.";
 
-    $headers = "From: $fromemail \n";
-    $headers .= "Reply-To: $email \n";
-
-    mail($myemail, $subject, $body_message, $headers);
-    header("Location: index.html");
+    mail("duvivierbri@gmail.com", "New Email From Your Website!", $body_message);
 ?>
